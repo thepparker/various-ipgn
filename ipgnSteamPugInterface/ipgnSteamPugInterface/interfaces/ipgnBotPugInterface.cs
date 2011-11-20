51ea719799da031a71acd0945780af3835321ee4
Prithu Parker <prithu@iinet.net.au>
2011-11-20 20:19:00 +1100

    completed socket support (auto reconnect) and added first part of data object

--- ipgnSteamPugInterface/ipgnSteamPugInterface/interfaces/ipgnBotPugInterface.cs
+++ ipgnSteamPugInterface/ipgnSteamPugInterface/interfaces/ipgnBotPugInterface.cs
@@ -5,10 +5,13 @@
     using System.Text;
     using System.Net;
     using System.Net.Sockets;
+    using System.Windows.Forms;
+    using System.Threading;
 
     class ipgnBotPugInterface
     {
         Socket ipgnPugInterfaceSocket;
+        CPUGData pugStatus;
 
         private const string ConnectionFailed = "Unable to connect to the pug bot";
         private const string ConnectionSuccess = "Connected to the pug bot";
@@ -16,13 +19,27 @@
 
         public bool connectedToBot;
 
+        public static IPEndPoint botIPEndPoint;
+        public static string botPassword;
+
         public ipgnBotPugInterface()
         {
             ipgnPugInterfaceSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            pugStatus = new CPUGData();
         }
+
+        /* Socket setup and control, including reading/receiving/connecting
+         * This is essential for connecting to the pug bot, however this method is not set in stone.
+         * I think perhaps a UDP listen socket on both this bot and the pug bot may be better than a
+         * constantly open TCP socket on the localhost, though no bandwidth cost will be induced either way
+         * and TCP is substantially more reliable.
+         */
 
         public bool connectToPugBot(IPEndPoint pugBotAddress, string interfacePassword)
         {
+            botIPEndPoint = pugBotAddress;
+            botPassword = interfacePassword;
+
             try
             {
                 ipgnPugInterfaceSocket.Connect(pugBotAddress);
@@ -35,9 +52,9 @@
             }
 
             onConnectionSuccess(true);
-            //ipgnPugInterfaceSocket.Blocking = false;
+            //ipgnPugInterfaceSocket.Blocking = false; -- we don't need non blocking, we're using async methods
 
-            //send start notice
+            //send start notice to bot with password
             sendBotCommand("STARTUP!" + interfacePassword);
 
             //start listening
@@ -48,6 +65,9 @@
 
         private void onSocketError(string error)
         {
+            //if (String.Compare(error, ConnectionClosed) == 0)
+            //    connectToPugBot(botIPEndPoint, botPassword);
+            
             Program.logToWindow("Socket had error: " + error);
         }
         private void onConnectionSuccess(bool connected)
@@ -76,7 +96,15 @@
             {
                 Program.logToWindow("Unable to send data - lost socket connection. Attempting to reconnect");
 
-                //TODO: Write automatic reconnect method
+                connectToPugBot(botIPEndPoint, botPassword);
+                Thread.Sleep(4000);
+                if (!connectedToBot)
+                {
+                    MessageBox.Show("Can't reconnect. Closing");
+                    Application.Exit();
+                }
+                byte[] commandPacket = ASCIIEncoding.ASCII.GetBytes(command);
+                ipgnPugInterfaceSocket.BeginSend(commandPacket, 0, commandPacket.Length, SocketFlags.None, new AsyncCallback(sendBotCallback), this);
             }
         }
 
@@ -143,7 +171,7 @@
                         SocketFlags.None, new AsyncCallback(receiveCallback), currentState);
                 else
                 {
-                    reactToBot(currentMessage);
+                    reactToBot(currentMessage, pugStatus);
                     receiveBotCommand(); //start listening again
                 }
             }
@@ -156,15 +184,23 @@
 
                 Program.logToWindow("Received data: " + receivedMessage);
 
-                //parse this command and react
-                reactToBot(receivedMessage);
+                //parse this command and react, sending the current status object with it
+                reactToBot(receivedMessage, pugStatus);
 
                 //go back to listening again
                 receiveBotCommand();
             }
         }
 
-        public void reactToBot(string botMessage)
+        /* End the socket control. Now we setup a function for processing the command send between the
+         * bots. The main program will need to be included in this control somehow, because it controls both
+         * the steam interface and the pug interface, and we need both for this bot to function properly.
+         * I think perhaps it may be better to initialize the socket inside the steam interface class, rather
+         * than having the main program control both. Though this could just be due to bad implementation ;)
+         */
+
+
+        public void reactToBot(string botMessage, CPUGData pugStatus)
         {
             //hardcode reactions here. ie, details, endpug, start/end mapvote, fun stuff
         }
@@ -180,5 +216,29 @@
 
         public int bytesRead;
         public bool IsNewData;
+    }
+
+
+    /* Our main data container for the pug bot interface. This will contain whether the pug is in progress,
+     * the number of players, the players joined through steam, their steamid, map vote, etc.
+     * Need to keep in mind, however, we don't want to replicate the pug bot's functions entirely, we want to
+     * emulate them
+     */
+    public class CPUGData 
+    {
+        //bools
+        public bool inProgress;
+        public bool mapVoting;
+        public bool detailed;
+
+        //ints
+        public int numPlayers;
+        public int serverPort;
+
+        //strings
+        public string serverIP;
+        public string serverPassword;
+        public string adminLogin;
+        public string[] mapVotes;
     }
 }
